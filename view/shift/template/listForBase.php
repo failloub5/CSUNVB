a659416785b508e8a34262ed1fc4e208d63afc17
Paola Costa <paola.costa98@gmail.com>
2020-12-17 09:06:23 +0100

    reformatage du fichier et modification de la syntaxe Php

--- view/shift/template/listForBase.php
+++ view/shift/template/listForBase.php
@@ -10,31 +10,40 @@ ob_start();
         <th>Équipage</th>
         <th>Action</th>
         </thead>
-        <?php foreach ($shiftsheets as $shiftsheet) { ?>
+        <?php foreach ($shiftsheets as $shiftsheet) : ?>
             <tr>
                 <td><a href='?action=showshift&id=<?= $shiftsheet['id'] ?>'
                        class="btn"><?= date('d.m.Y', strtotime($shiftsheet['date'])) ?>  </a></td>
                 <td>
                     <?= $shiftsheet['status'] ?>
                 </td>
-                <td>Jour : <?= $shiftsheet['novaDay'] ?><br>Nuit : <?= $shiftsheet['novaNight'] ?></td>
-                <td>Jour : <?= $shiftsheet['bossDay'] ?><br>Nuit : <?= $shiftsheet['bossNight'] ?> </td>
-                <td>Jour : <?= $shiftsheet['teammateDay'] ?><br>Nuit : <?= $shiftsheet['teammateNight'] ?></td>
+                <td>
+                    Jour : <?= $shiftsheet['novaDay'] ?><br>Nuit : <?= $shiftsheet['novaNight'] ?>
+                </td>
+                <td>
+                    Jour : <?= $shiftsheet['bossDay'] ?><br>Nuit : <?= $shiftsheet['bossNight'] ?>
+                </td>
+                <td>
+                    Jour : <?= $shiftsheet['teammateDay'] ?><br>Nuit : <?= $shiftsheet['teammateNight'] ?>
+                </td>
                 <td>
                     <!-- TODO (XCL): faire un helper qui donne l'action correspondante à l'état actuel -->
-                    <?php if ((($_SESSION['user']['admin'] == true and getNbshiftsheet('open', $baseID) == 0) ||
-                        ($_SESSION['user']['admin'] == true and $shiftsheet['statusslug'] == 'close') ||
-                        $shiftsheet['statusslug'] == 'open' ||
-                        $shiftsheet['statusslug'] == 'reopen')) { ?>
+                    <?php if ((
+                            ($_SESSION['user']['admin'] == true and getNbshiftsheet('open', $baseID) == 0) ||
+                            ($_SESSION['user']['admin'] == true and $shiftsheet['statusslug'] == 'close') ||
+                            $shiftsheet['statusslug'] == 'open' ||
+                            $shiftsheet['statusslug'] == 'reopen')) : ?>
                         <form>
                             <input type="hidden" name="action" value="altershiftsheetStatus">
                             <input type=hidden name="id" value= <?= $shiftsheet['id'] ?>>
-                            <button class="btn btn-primary btn-sm"><?= actionForStatus($shiftsheet['statusslug'])?></button>
+                            <button class="btn btn-primary btn-sm">
+                                <?= actionForStatus($shiftsheet['statusslug']) ?>
+                            </button>
                         </form>
-                    <?php } ?>
+                    <?php endif; ?>
                 </td>
             </tr>
-        <?php } ?>
+        <?php endforeach; ?>
     </table>
 <?php
 $content = ob_get_clean();
