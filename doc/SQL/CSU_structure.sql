d1fbfbab02bcf1453a9f22f1c63a0b7068f3f2d6
MichaelGogniat <michael.gogniat@cpnv.ch>
2021-01-07 12:21:43 +0100

    supression d'un duplicatat dans le script mysql

--- doc/SQL/CSU_structure.sql
+++ doc/SQL/CSU_structure.sql
@@ -6,9 +6,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema csunvb_csu
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema csunvb_csu
@@ -88,17 +85,6 @@ CREATE TABLE IF NOT EXISTS `csunvb_csu`.`users` (
   `firstconnect` TINYINT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `initials_UNIQUE` (`initials` ASC) )
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `csunvb_csu`.`status`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `csunvb_csu`.`status` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `slug` VARCHAR(10) UNIQUE NOT NULL,
-  `displayname` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
